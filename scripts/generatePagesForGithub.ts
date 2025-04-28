9aca37820b782422d1d78d69e6850a29455b10d4
Simon <sgr@ge.no>
2025-04-28 17:57:41 +0200

    Added script tog create pages for github pages

---
+++ scripts/generatePagesForGithub.ts
@@ -0,0 +1,21 @@
+import fs from "fs-extra";
+import * as path from "node:path";
+
+const filePath = path.join('dist', "about", 'index.html');
+// @ts-ignore
+await fs.ensureDir(path.dirname(filePath));
+// @ts-ignore
+await fs.writeFile(filePath,`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Generated about</h1>
+</body>
+</html>
+` );
+console.log("Successfully generated test page");
+
