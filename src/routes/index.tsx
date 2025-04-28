220f01cc94258c1d007177133395c5d61260e66b
Simon <sgr@ge.no>
2025-04-28 15:37:14 +0200

    Update?

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ function App() {
     // const accessToken, setAccessToken] = useState<string>()
     return (
         <div className="App">
-            <h1>Hello</h1>
+            <h1>Hello. kjær?</h1>
             {/*<button onClick={()=>{*/}
             {/*    loginToGithub().then(async ()=>{*/}
             {/*        var fetchedAccesstoken = await getGithubAccessToken();*/}
