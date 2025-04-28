f51e98792ba9a243f95716adca58bd324aa5f16c
Simon <sgr@ge.no>
2025-04-28 15:44:26 +0200

    Added back login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,33 @@
 import {createFileRoute} from '@tanstack/react-router'
-// import {useState} from "react";
-// import {getGithubAccessToken, loginToGithub} from "@/utils/github/login.ts";
+import {useEffect, useState} from "react";
+import {getGithubAccessToken, loginToGithub} from "@/utils/github/login.ts";
 
 export const Route = createFileRoute('/')({
     component: App,
 })
 
 function App() {
-    // const accessToken, setAccessToken] = useState<string>()
+    const [accessToken, setAccessToken] = useState<string>()
+    useEffect(() => {
+        (async ()=> {
+            const token = await getGithubAccessToken()
+            setAccessToken(token)
+        })()
+    },[])
     return (
         <div className="App">
             <h1>Hello. kjær? ddddd</h1>
-            {/*<button onClick={()=>{*/}
-            {/*    loginToGithub().then(async ()=>{*/}
-            {/*        var fetchedAccesstoken = await getGithubAccessToken();*/}
-            {/*        if(fetchedAccesstoken){*/}
-            {/*            setAccessToken(fetchedAccesstoken)*/}
-            {/*        }*/}
-            {/*    })*/}
-            {/*}}>Login to github</button>*/}
+            <button onClick={()=>{
+                loginToGithub().then(async ()=>{
+                    var fetchedAccesstoken = await getGithubAccessToken();
+                    if(fetchedAccesstoken){
+                        setAccessToken(fetchedAccesstoken)
+                    }
+                })
+            }}>Login to github</button>
 
-            {/*{!accessToken && <div>Missing accesstoken</div>}*/}
-            {/*{accessToken && <div>Accesstoken: {accessToken}</div>}*/}
+            {!accessToken && <div>Missing accesstoken</div>}
+            {accessToken && <div>Accesstoken: {accessToken}</div>}
         </div>
     )
 }
